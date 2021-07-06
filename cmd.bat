2d67b15bcb1787a591ee4adedb72fc1fee088d2b
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-07-06 13:20:35 +0530

    avoided test files

--- cmd.bat
+++ cmd.bat
@@ -1,9 +1,12 @@
-python main-setup.py --help
+rem python main-setup.py --help
+rem python main-setup.py --ireqfile <file_name>
 python main-setup.py --touch Pipfile
 python main-setup.py --install requirments.txt
-python main-setup.py --package django-sslserver
-python main-setup.py --package django-registration-redux
-python main-setup.py --package django-crispy-forms
+rem python main-setup.py --package django-sslserver
+rem python main-setup.py --package django-registration-redux
+rem python main-setup.py --package django-crispy-forms
+rem python main-setup.py --package whitenoise
+rem python main-setup.py --package gunicorn
 python main-setup.py --project main
 python main-setup.py --cred
 python main-setup.py --app main,dataStorage
@@ -16,10 +19,7 @@ python main-setup.py --copy temp/static,src/static
 python main-setup.py --copy temp/templates,src/templates
 python main-setup.py --replace src/main/urls.py,]\n,\tpath\('accounts/',include\('registration.backends.simple.urls'\)\),\n
 python main-setup.py --folder src/media
-python main-setup.py --package whitenoise
-python main-setup.py --package gunicorn
 python main-setup.py --heroku main,python-3.9.5
 python main-setup.py --clean
 python main-setup.py --setup --secure main --run 1
-
-python main-setup.py --ireqfile <file_name>
+pause
