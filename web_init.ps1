602269399bffffa9bc713c5183512db11bbaa7b8
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-07-09 00:57:03 +0530

    testing: web install attempt 2

--- web_init.ps1
+++ web_init.ps1
@@ -1,3 +1,23 @@
-powershell.exe -Command "InvokeWebRequest (https://github.com/rshnGhost/eSpace-lazy.git) -Outfile eSpace-lazy.zip"
-powershell.exe -NoP -NonI -Command "Expand-Archive '.\eSpace-lazy.zip' '.\eSpace-lazy\'"
-pause
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$ErrorActionPreference = 'SilentlyContinue'
+$wshell = New-Object -ComObject Wscript.Shell
+$Button = [System.Windows.MessageBoxButton]::YesNoCancel
+$ErrorIco = [System.Windows.MessageBoxImage]::Error
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
+}
+
+# GUI Specs
+Write-Host "Checking winget..."
+
+# winget is not installed. Install it from the Github release
+Write-Host "winget is not found, installing it right now."
+
+$download = "https://github.com/rshnGhost/eSpace-lazy.git"
+$output = $PSScriptRoot + "\eSpace-lazy.zip"
+Write-Host "Dowloading latest release"
+Invoke-WebRequest -Uri $download -OutFile $output
+Expand-Archive $output '.\eSpace-lazy\'
