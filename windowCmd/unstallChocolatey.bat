4d33ed6464b186ebc937f9ec20a68610d28d00da
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-07-09 23:50:27 +0530

    fixed: one click install choco

--- windowCmd/unstallChocolatey.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -WorkingDirectory '%~dp0' -Verb RunAs}"
-PAUSE
