9ba6565018352e413c72802f2fbb3d262f9ee151
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2021-07-09 14:38:00 +0530

    Update webInstall.ps1

--- webInstall.ps1
+++ webInstall.ps1
@@ -26,7 +26,7 @@ If (!$status) {
 } else {
   Write-Host "Executing..."
   cd 'C:\Temp\eSpace-lazy-django-3.2.5\windowCmd\'
-  & 'C:\Temp\eSpace-lazy-django-3.2.5\delete.bat
+  & 'C:\Temp\eSpace-lazy-django-3.2.5\delete.bat'
   & 'C:\Temp\eSpace-lazy-django-3.2.5\windowCmd\2 setup.bat'
   & 'C:\Temp\eSpace-lazy-django-3.2.5\windowCmd\3 run.bat'
 }
