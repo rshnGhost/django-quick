3e0d6dc793f7ab4db528a5129c4eae2b1bc1c73b
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2022-12-26 11:18:40 +0530

    removed: os dependent

--- push.bat
+++
@@ -1,9 +0,0 @@
-git init
-git remote add origin https://github.com/rshnGhost/eSpace-lazy.git
-git branch -m django-4.1.4
-git status
-git add .
-git status
-git commit -m %1
-git switch django-4.1.4
-git push origin django-4.1.4
